ace5b0b29179024dfa9e81ec68fde0051433c3d0
LilBuffy <davidczamora1@gmail.com>
2026-08-12 11:31:01 +0800

    Delete library_management directory

--- library_management/library_management/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </div><!-- /.page-content -->
-</div><!-- /.main-content -->
-</div><!-- /.wrapper -->
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?= BASE_URL ?>/assets/js/app.js"></script>
-</body>
-</html>
